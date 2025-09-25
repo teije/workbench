54395f054a0c0c39c435da4c5819b02431b63e98
Teije van Hemert <teijevhemert@gmail.com>
2025-09-25 15:07:18 +0200

    Initial commit

---
+++ src/Workbench.Api/Controllers/StatusController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Workbench.Api.Controllers;
+
+[Route("api/status")]
+[ApiController]
+public class StatusController : ControllerBase
+{
+    [HttpGet("ping")]
+    public ActionResult<string> Get()
+    {
+        return Ok("pong");
+    }
+}
